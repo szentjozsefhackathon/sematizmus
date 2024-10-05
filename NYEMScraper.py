5ee2a5e55ef922109854eb7d652d13603eb1871d
Vörös László <vorosl@inf.u-szeged.hu>
2024-10-05 15:56:34 +0200

    NYEM details

--- NYEMScraper.py
+++ NYEMScraper.py
@@ -4,6 +4,28 @@ import re
 from tqdm import tqdm
 import  json
 import argparse
+import datetime
+import re
+import urllib3
+urllib3.disable_warnings(category=urllib3.exceptions.InsecureRequestWarning)
+honapok = {
+    "január": 1,
+    "február": 2,
+    "március": 3,
+    "április": 4,
+    "május": 5,
+    "június": 6,
+    "július": 7,
+    "augusztus": 8,
+    "szeptember": 9,
+    "október": 10,
+    "november": 11,
+    "december": 12
+}
+
+def str2date(datum):
+    reszek = [d.split(".")[0].strip() for d in datum.strip().split(" ")]
+    return datetime.date(int(reszek[0]), honapok[reszek[1]], int(reszek[2]))
 
 def NYEM(filename=None, year=None):
     # Replace this with the URL of the website you want to scrape
@@ -45,22 +67,46 @@ def NYEM(filename=None, year=None):
 
         soup = BeautifulSoup(html_content, 'html.parser')
         if "Elhunyt:" in soup.text: continue
-        
+        if not "Pappá szentelés" in soup.text and not "Diakónussá szentelés" in soup.text: continue
         imgSrc = ""
         try:
             imgSrc = "https://nyirgorkat.hu" + soup.select_one("img.indexpap").get("src")
         except:
             pass
+        birth = None
+        ordination = None
+
+        for r in soup.select("tr"):
+            try:
+                if "Születés helye" in r.text:
+                    birth = str2date(r.text.split(",")[-1])
+                if "Pappá szentelés" in r.text:
+                    ordination = str2date(r.text.split(",")[-1])
+                if "Diakónussá szentelés" in r.text and not "Pappá szentelés" in soup.text:
+                    ordination = str2date(r.text.split(",")[-1])
+            except:
+                print(soup.select_one("#parokianev").text)
+            
+        retired = True
+        for s in soup.select(".szolgalat td td"):
+            if re.search("-<td>", str(s)): retired = False
+
+        if retired == False and "nyugállományban" in soup.text.lower(): retired = True
 
         paplista.append({
             "name": soup.select_one("#parokianev").text, # A pap neve
             "img": imgSrc, # A kép linkje,
-            "src": f"https://nyirgorkat.hu/{pap}"
+            "src": f"https://nyirgorkat.hu/{pap}",
+            "birth": birth,
+            "ordination": ordination,
+            "deacon": not "Pappá szentelés" in soup.text,
+            "bishop": "Szocska A. Ábel" in soup.select_one("#parokianev").text,
+            "retired": retired
         })
     if filename == None: return paplista
     else:
         with open(filename, "w") as outfile:
-            outfile.write(json.dumps(paplista))
+            outfile.write(json.dumps(paplista, default=str))
 
 
 if __name__ == "__main__":
