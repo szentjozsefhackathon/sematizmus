2f481b54bfaa81cb7a06f9308506f5730b4e4878
Vörös László <vorosl@inf.u-szeged.hu>
2024-10-05 22:19:31 +0200

    Display last update fix

--- lastUpdate.py
+++ lastUpdate.py
@@ -2,4 +2,4 @@ import json
 import datetime
 
 with open("lastUpdate.json", "w") as f:
-    f.write(json.dumps({"lastUpdate": datetime.now()}, default=str))
+    f.write(json.dumps({"lastUpdate": datetime.datetime.now()}, default=str))
