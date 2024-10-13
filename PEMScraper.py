af92bc03d8108b5a15b1a057ef1356755dda3bc9
Vörös László <vorosl@inf.u-szeged.hu>
2024-10-13 22:26:27 +0200

    PEM multiprocess

--- PEMScraper.py
+++ PEMScraper.py
@@ -5,6 +5,9 @@ from tqdm import tqdm
 import json
 import argparse
 import datetime
+from tqdm.contrib.concurrent import process_map
+import urllib3
+urllib3.disable_warnings(category=urllib3.exceptions.InsecureRequestWarning)
 honapok = {
         "január": 1,
         "február": 2,
@@ -22,6 +25,54 @@ honapok = {
 def str2date(datum):
     reszek = [d.split(".")[0].strip() for d in datum.split(" ")]
     return datetime.date(int(reszek[0]), honapok[reszek[1]], int(reszek[2]))
+
+def processPriest(link):
+        try: # Kétszeri próbálkozásra szokott menni
+            response = requests.get(link, verify=False)
+            if response.status_code == 200:
+                html_content = response.content
+            else:
+                print("Failed to fetch the website.")
+        except:
+            try:
+                response = requests.get(link, verify=False)
+                if response.status_code == 200:
+                    html_content = response.content
+                else:
+                    print("Failed to fetch the website.")
+            except:
+                print("Big error")
+                return
+
+
+        soup = BeautifulSoup(html_content, 'html.parser')
+        imgSrc = ""
+        try:
+            imgSrc = "https://pecsiegyhazmegye.hu" + soup.select_one(".item-page img").get("src")
+        except:
+            pass
+        birth = None
+        ordination = None
+        for sor in soup.select_one(".kpriest-content-right table").findAll("tr"): # Papi táblázat
+            if(sor.select_one("th").text == "Született"): 
+                birth = str2date(sor.select_one("td").text.strip().split(", ")[1])
+            
+            if(sor.select_one("th").text == "Szentelés"):
+                try:
+                    ordination = str2date(sor.select_one("td").text.strip().split(", ")[1])
+                except:
+                    ordination = str2date(sor.select_one("td").text.strip())
+        return {
+            "name": soup.select_one(".page-header h2").text, # A pap neve
+            "birth": birth,
+            "ordination": ordination,
+            "img": imgSrc, # A kép linkje,
+            "src": link,
+            "retired": "nyugállományban" in soup.text or "ny. megyéspüspök" in soup.text,
+            "bishop": "megyéspüspök" in soup.text,
+            "deacon": "diakónus" in soup.text
+        }
+
 
 
 def PEM(filename=None, year=None):
@@ -46,53 +97,9 @@ def PEM(filename=None, year=None):
 
         papok.append(sor.findAll('td')[0].select_one('a')['href']) # Papi oldalak linkjei
 
-    paplista = []
-    for pap in tqdm(papok): # Nézze meg az összes pap linkjét
-        try: # Kétszeri próbálkozásra szokott menni
-            response = requests.get(pap, verify=False)
-            if response.status_code == 200:
-                html_content = response.content
-            else:
-                print("Failed to fetch the website.")
-        except:
-            try:
-                response = requests.get(pap, verify=False)
-                if response.status_code == 200:
-                    html_content = response.content
-                else:
-                    print("Failed to fetch the website.")
-            except:
-                print("Big error")
-                continue
+    paplista = process_map(processPriest, papok)
+    paplista = [p for p in paplista if p != None]
 
-
-        soup = BeautifulSoup(html_content, 'html.parser')
-        imgSrc = ""
-        try:
-            imgSrc = "https://pecsiegyhazmegye.hu" + soup.select_one(".item-page img").get("src")
-        except:
-            pass
-        birth = None
-        ordination = None
-        for sor in soup.select_one(".kpriest-content-right table").findAll("tr"): # Papi táblázat
-            if(sor.select_one("th").text == "Született"): 
-                birth = str2date(sor.select_one("td").text.strip().split(", ")[1])
-            
-            if(sor.select_one("th").text == "Szentelés"):
-                try:
-                    ordination = str2date(sor.select_one("td").text.strip().split(", ")[1])
-                except:
-                    ordination = str2date(sor.select_one("td").text.strip())
-        paplista.append({
-            "name": soup.select_one(".page-header h2").text, # A pap neve
-            "birth": birth,
-            "ordination": ordination,
-            "img": imgSrc, # A kép linkje,
-            "src": pap,
-            "retired": "nyugállományban" in soup.text or "ny. megyéspüspök" in soup.text,
-            "bishop": "megyéspüspök" in soup.text,
-            "deacon": "diakónus" in soup.text
-        })
     if filename == None: return paplista
     else:
         with open(filename, "w") as outfile:
