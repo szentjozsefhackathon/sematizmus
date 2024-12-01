828bba5e010c8b1da10ecfb85d9ecf8591453c7a
Vörös László <vorosl@inf.u-szeged.hu>
2024-12-01 23:27:36 +0100

    fix orderAbbreviation

--- orderAbbreviation.py
+++ orderAbbreviation.py
@@ -6,38 +6,37 @@ def orderAbbreviation(wrapped_function):
         @wraps(wrapped_function)
         def _wrapper(*args, **kwargs):
             results = wrapped_function(*args, **kwargs)
-            results = [r for r in results if "SJ" in r["name"] and not "SJP" in r["name"]]
+            results = [r for r in results if not ("SJ" in r["name"] and not "SJP" in r["name"])]
             new_results = []
-            orderAbbreviations = ["SJP", "OFM Conv.", "OSB", "OP", "SDS", "SVD", "FSCB", "OSPPE", "FSO", "SDB", "OCD", "OH", "TORG", "SMC"]
+            orderAbbreviations = ["SJP", "OFM Conv.", "OSB", "OP", "SDS", "SVD", "FSCB", "OSPPE", "FSO", "SDB", "OCD", "OH", "TORG", "SMC", "OFM", "MI", "CM"]
 
             for r in results:
-
-                for oa in orderAbbreviations:
-                    if oa in r["name"]:
-                        r["name"] = r["name"].replace(oa, "")
-                        r["orderAbbreviation"] = oa
-
                 if "O.Praem." in r["name"] or "O.Praem" in r["name"] or "OPraem" in r["name"]:
-                    r["name"] = r["name"].replace("O.Praem.", "").replace("O.Praem", "")
+                    r["name"] = r["name"].replace("O.Praem.", "").replace("O.Praem", "").replace("OPraem", "")
                     r["orderAbbreviation"] = "OPraem"
 
                 if "O.Cist." in r["name"] or "O. Cist" in r["name"] or "O.Cist" in r["name"]:
-                    r["name"] = r["name"].replace("O.Cist.", "").replace("O. Cist", "")
+                    r["name"] = r["name"].replace("O.Cist.", "").replace("O. Cist", "").replace("O.Cist", "")
                     r["orderAbbreviation"] = "OCist"
 
                 if "OFMCap" in r["name"] or "OFM Cap" in r["name"] or "OFM Cap." in r["name"]:
                     r["name"] = r["name"].replace("OFMCap", "").replace("OFM Cap.", "").replace("OFM Cap", "")
                     r["orderAbbreviation"] = "OFMCap"
 
-                if "SchP" in r["name"] or "SP" in r["name"] or "Sch. P" in r["name"]:
+                if ("SchP" in r["name"] or "SP" in r["name"] or "Sch. P" in r["name"] ) and not "OSPPE" in r["name"]:
                     r["name"] = r["name"].replace("SchP", "").replace("SP", "").replace("Sch. P", "")
                     r["orderAbbreviation"] = "SP"
 
-                if "Schr." in r["name"]:
-                    r["name"] = r["name"].replace("Schr.", "")
-                    r["orderAbbreviation"] = "Schr"
+                if "SChr." in r["name"]:
+                    r["name"] = r["name"].replace("SChr.", "")
+                    r["orderAbbreviation"] = "SChr"
 
-
+                for oa in orderAbbreviations:
+                    if oa in r["name"]:
+                        r["name"] = r["name"].replace(oa, "")
+                        r["orderAbbreviation"] = oa
+                
+                r["name"] = r["name"].strip()
                 new_results.append(r)
                 
             return new_results
