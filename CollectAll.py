9e7400dfc842b2d4dbf06668e47820241f4a7bd1
Vörös László <vorosl@inf.u-szeged.hu>
2024-12-16 21:16:28 +0100

    Multiplications, papima

--- CollectAll.py
+++ CollectAll.py
@@ -24,8 +24,16 @@ from SPScraper import SP
 
 import json
 import argparse
-import numpy
 
+def deleteMultiplications(priests):
+    with open("multiplications.json", "r") as infile:
+        multiplications = json.load(infile)
+    for multiplication in multiplications:
+        if multiplication[0] in priests:
+            for i in range(1, len(multiplication)):
+                if multiplication[i] in priests:
+                    priests.remove(multiplication[i])
+    return priests
 def priestList(year, filename=None): 
     _dioceses = {
         "Esztergom-Budapesti főegyházmegye": EBFEM(year=year),
@@ -67,7 +75,7 @@ def priestList(year, filename=None):
                 "orderAbbreviation": priest.get("orderAbbreviation"),
                 "doctor": priest.get("doctor")
             })
-
+    priests = deleteMultiplications(priests)
     if filename == None:
         return priests
     else:
---
+++ papima.py
@@ -0,0 +1,17 @@
+import json
+
+def papima(datafile):
+    with open(datafile, "r") as infile:
+        priests = json.load(infile)
+    
+    priests.sort(key=lambda x: x["name"])
+    priests = list({v['src']:v for v in priests}.values())
+
+
+    priests = list(filter(lambda x: "deacon" in x and x["deacon"] != True, priests))
+
+    with open("papima.json", "w") as outfile:
+        json.dump(priests, outfile, indent=4)
+
+if __name__ == "__main__":
+    papima("data.json")
